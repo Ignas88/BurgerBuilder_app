88304a95dee8b3ed43d68888b49f405b121fd71b
Ignas <ignas.cepelis@presentconnection.eu>
2018-08-06 15:23:25 +0300

    sidedrawer created

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import Navigationitems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ...
+        <div className={classes.Logo}>
+         <Logo/>
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <Navigationitems />
         </nav>
     </header>
 );
